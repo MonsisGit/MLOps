34ea8f4f195b07f6a53afd98ebc40744ec589022
Simon <simon.goldhofer@tutanota.com>
2022-01-04 14:12:21 +0100

    fuck wsl

--- exercise_files/final_exercise/data.py
+++ exercise_files/final_exercise/data.py
@@ -1,8 +1,24 @@
-import torch
-
-
+from torch import Tensor, randn
+import numpy as np
+from torchvision import datasets, transforms
+from pathlib import Path
+from torch.utils.data import TensorDataset, DataLoader
 def mnist():
-    # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
+
+    # Download and load the training data
+    path_1 = str(Path.cwd().parent.parent) + "/dtu_mlops/data/corruptmnist/train_0.npz"
+    trainset = np.load(path_1)
+    images = Tensor(trainset["images"]) # transform to torch tensor
+    labels = Tensor(trainset["labels"])
+    my_dataset = TensorDataset(images,labels)
+    train = DataLoader(my_dataset, batch_size=32, shuffle=True)
+    path_2 = str(Path.cwd().parent.parent) + "/dtu_mlops/data/corruptmnist/test.npz"
+    testset = np.load(path_2)
+    images = Tensor(testset["images"]) # transform to torch tensor
+    labels = Tensor(testset["labels"])
+    my_dataset = TensorDataset(images,labels)
+    test = DataLoader(my_dataset, batch_size=32, shuffle=True)    
     return train, test
+    
+if __name__ == '__main__':
+    mnist()
--- exercise_files/final_exercise/main.py
+++ exercise_files/final_exercise/main.py
@@ -5,7 +5,10 @@ import torch
 
 from data import mnist
 from model import MyAwesomeModel
-
+from torch import optim
+from torch import nn
+import numpy as np
+import matplotlib.pyplot as plt
 
 class TrainOREvaluate(object):
     """ Helper class that will help launch class methods as commands
@@ -37,7 +40,28 @@ class TrainOREvaluate(object):
         # TODO: Implement training loop here
         model = MyAwesomeModel()
         train_set, _ = mnist()
+        criterion = nn.NLLLoss()
+        optimizer = optim.SGD(model.parameters(), lr=0.003)
+        epochs = 50
+        for e in range(epochs):
+            running_loss = 0
+            for images, labels in train_set:
+                # Flatten MNIST images into a 784 long vector
+                optimizer.zero_grad()
+                # TODO: Training pass
+                output = model(images)
+                loss = criterion(output.squeeze(),labels.long())
+                loss.backward()        
+                running_loss += loss.item()
+                optimizer.step()
+               # import pdb
+               # pdb.set_trace()
+            else:
+                print(f"Training loss: {running_loss/len(train_set)}")
+                
+        torch.save(model.state_dict(),'checkpoint.pth')
         
+    
     def evaluate(self):
         print("Evaluating until hitting the ceiling")
         parser = argparse.ArgumentParser(description='Training arguments')
@@ -45,10 +69,24 @@ class TrainOREvaluate(object):
         # add any additional argument that you want
         args = parser.parse_args(sys.argv[2:])
         print(args)
-        
         # TODO: Implement evaluation logic here
         model = torch.load(args.load_model_from)
-        _, test_set = mnist()
+        _, testloader = mnist()
+        model = MyAwesomeModel()
+        criterion = nn.NLLLoss()        
+        with torch.no_grad():
+            cnt = []
+            running_loss = 0
+            for images, labels in testloader:       
+                log_ps = model(images)
+                loss = criterion(log_ps, labels.long())
+                running_loss += loss.item()
+                _, top_class = torch.exp(log_ps).topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                cnt.append(torch.mean(equals.type(torch.FloatTensor)))  
+            acc = torch.sum(torch.tensor(cnt))/len(cnt)
+            print(f'Accuracy: {acc*100:.2f}%')
+
 
 if __name__ == '__main__':
     TrainOREvaluate()
