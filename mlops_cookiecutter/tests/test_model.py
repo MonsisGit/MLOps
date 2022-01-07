03f944e79a4fc7b46d044ef33c0aca75b430d10f
Simon <simon.goldhofer@tutanota.com>
2022-01-07 17:02:24 +0100

    added

---
+++ mlops_cookiecutter/tests/test_model.py
@@ -0,0 +1,29 @@
+import torch
+import os
+import os
+import sys
+import pytest
+from torch.nn.modules import module
+  
+# setting path
+sys.path.append(os.getcwd())
+from src.models.build_model import MyModel
+from src.models.train_model import train
+
+
+@pytest.mark.parametrize("batch_size,expected_dim", [(1,1), (10,10)])
+def test_model(batch_size,expected_dim):
+    data_train_path = os.getcwd() + "/data/processed/train.pth"
+    model_path = os.getcwd() + "/models/checkpoint.pth"
+
+    trainloader = torch.load(data_train_path)
+    assert trainloader.dataset.tensors[0].shape==torch.Size([5000,28,28])
+    model = MyModel()
+    model.load_state_dict(torch.load(model_path))
+    assert model(trainloader.dataset.tensors[0][0:batch_size].view(batch_size,-1)).shape==torch.Size([expected_dim,10])
+
+def test_training():
+    try:
+        train(1,False)
+    except Exception as e:
+        print(f"Training failed with: \n {e}")
