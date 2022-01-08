55c331d4f65ae07df191ff035d86f663c13eae57
Simon <simon.goldhofer@tutanota.com>
2022-01-08 14:47:02 +0100

    auto_tests

--- mlops_cookiecutter/tests/test_data.py
+++ mlops_cookiecutter/tests/test_data.py
@@ -1,16 +1,12 @@
 import torch
 import os
 
-data_train_path = os.getcwd() + "/data/processed/train.pth"
-trainloader = torch.load(data_train_path)
-data_test_path = os.getcwd() + "/data/processed/train.pth"
-testloader = torch.load(data_train_path)
 
 def test_dataset():
-    data_train_path = os.getcwd() + "/data/processed/train.pth"
+    data_train_path = os.getcwd() + "/mlops_cookiecutter/data/processed/train.pth"
     trainloader = torch.load(data_train_path)
-    data_test_path = os.getcwd() + "/data/processed/train.pth"
-    testloader = torch.load(data_train_path)
+    data_test_path = os.getcwd() + "/mlops_cookiecutter/data/processed/train.pth"
+    testloader = torch.load(data_test_path)
     assert len(trainloader) == 157
     assert len(testloader) == 157, f"Testdataset has shape {len(testloader)} but should be 1000"
 
--- mlops_cookiecutter/tests/test_model.py
+++ mlops_cookiecutter/tests/test_model.py
@@ -6,15 +6,15 @@ import pytest
 from torch.nn.modules import module
   
 # setting path
-sys.path.append(os.getcwd())
+sys.path.append(os.getcwd() + "/mlops_cookiecutter")
 from src.models.build_model import MyModel
 from src.models.train_model import train
 
 
 @pytest.mark.parametrize("batch_size,expected_dim", [(1,1), (10,10)])
 def test_model(batch_size,expected_dim):
-    data_train_path = os.getcwd() + "/data/processed/train.pth"
-    model_path = os.getcwd() + "/models/checkpoint.pth"
+    data_train_path = os.getcwd() + "/mlops_cookiecutter/data/processed/train.pth"
+    model_path = os.getcwd() + "/mlops_cookiecutter/models/checkpoint.pth"
 
     trainloader = torch.load(data_train_path)
     assert trainloader.dataset.tensors[0].shape==torch.Size([5000,28,28])
