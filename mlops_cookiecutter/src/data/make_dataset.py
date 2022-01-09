10fcb5c42c0e1a413f7a5db75265b5840e397398
Simon <simon.goldhofer@tutanota.com>
2022-01-09 17:12:56 +0100

    changeslol

--- mlops_cookiecutter/src/data/make_dataset.py
+++ mlops_cookiecutter/src/data/make_dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.index_tricks import _fill_diagonal_dispatcher
 from torch import Tensor, save
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -11,13 +12,13 @@ def mnist():
     images = Tensor(trainset["images"]) # transform to torch tensor
     labels = Tensor(trainset["labels"])
     my_dataset = TensorDataset(images,labels)
-    train = DataLoader(my_dataset, batch_size=32, shuffle=True,num_workers=8)
+    train = DataLoader(my_dataset, batch_size=32, shuffle=True,num_workers=4)
     path_2 = "data/raw/test.npz"
     testset = np.load(path_2)
     images = Tensor(testset["images"]) # transform to torch tensor
     labels = Tensor(testset["labels"])
     my_dataset = TensorDataset(images,labels)
-    test = DataLoader(my_dataset, batch_size=32, shuffle=True,num_workers=8)    
+    test = DataLoader(my_dataset, batch_size=32, shuffle=False,num_workers=4)    
     save(train,"data/processed/train.pth")
     save(test,"data/processed/test.pth")
     print("Saved to data/processed/")
--- mlops_cookiecutter/src/models/train_model_cml.py
+++ mlops_cookiecutter/src/models/train_model_cml.py
@@ -4,13 +4,15 @@ import sys
 import logging
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 
 sys.path.append(os.getcwd())
 from src.models.build_model import MyModel
 
 
 
-def train(epochs:int=20,save:bool=True) -> None:
+def train(epochs:int=1,save:bool=True) -> None:
     log = logging.getLogger(__name__)
     log.info("Training day and night")
     
@@ -26,6 +28,23 @@ def train(epochs:int=20,save:bool=True) -> None:
     logger = TensorBoardLogger("tb_logs", name="my_model")
     trainer = pl.Trainer(max_epochs=epochs,logger=logger)
     trainer.fit(model,trainloader,val_dataloaders=valloader)
+
+    preds, target = [], []
+    for batch in trainloader:
+        x, y = batch
+        probs = model(x)
+        preds.append(torch.exp(probs).argmax(dim=-1))
+        target.append(y.detach())
+    target = torch.cat(target, dim=0)
+    preds = torch.cat(preds, dim=0)
+
+    report = classification_report(target, preds)
+    with open("reports/classification_report.txt", 'w') as outfile:
+        outfile.write(report)
+        
+    ConfusionMatrixDisplay.from_predictions(
+    target, preds)
+    plt.savefig('reports/figures/confusion_matrix.png')
 
     if save:
         save_pth = os.getcwd() + '/models/checkpoint_new.pth'
